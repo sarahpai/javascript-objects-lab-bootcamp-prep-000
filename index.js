4df9941509e5a3aef266439c1f25d45793d3d740
Sarah Pai <pai.sarah89@gmail.com>
2018-08-25 19:48:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, {[key]: value});
 }
 
-function 
+function deleteFromObjectByKey(object, key){
+  return Object.assign({},{[key]: value})
+  delete object.key;
+}
